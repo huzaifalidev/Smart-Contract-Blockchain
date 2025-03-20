8087777bcd1d0b4ec4cc0bc5db3dd619f4362a46
huzaifa-Ali-Khan <huzaifalikhan48@gmail.com>
2025-03-20 13:24:52 +0500

    🚀 Add fifth lecture materials including token burn and mint functionality, and staking contract

---
+++ Staking1.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.22;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+
+contract MyToken is ERC20, ERC20Permit {
+    constructor(address recipient)
+        ERC20("MyToken", "MTK")
+        ERC20Permit("MyToken")
+    {
+        _mint(recipient, 100000 * 10 ** decimals());
+    }
+}
+
+contract Staking {
+    mapping (address =>uint) stakedToken;
+    MyToken public token;
+constructor(address _token){
+    token = MyToken(_token);
+}
+function stake(uint amount) public {
+token.transfer(address(this), amount);
+stakedToken[msg.sender]+=amount;
+}
+
+}
+
